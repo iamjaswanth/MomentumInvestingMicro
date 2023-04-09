038309738b61fc4bf307f17c8fb9c371f066838a
iamjaswanth <jaswanthsrm@gmail.com>
2023-04-09 14:23:44 +0530

    Create Momentum.py

---
+++ Momentum.py
@@ -0,0 +1,10 @@
+
+import streamlit as st
+import pandas as pd
+
+import requests
+from urllib.error import URLError
+
+
+st.title('Momentum Investing')
+st.header('The trend is your friend until the end, when it bends.')
